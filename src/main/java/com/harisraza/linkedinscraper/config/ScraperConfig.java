57d9e7a1d09901395063aae0327c7d625a7d300b
Muhammad Haris <wannaheadshot5@gmail.com>
2023-08-09 02:59:04 +0500

    fixed headless mode not working and implemented eager page loading

--- src/main/java/com/harisraza/linkedinscraper/config/ScraperConfig.java
+++ src/main/java/com/harisraza/linkedinscraper/config/ScraperConfig.java
@@ -1,6 +1,7 @@
 package com.harisraza.linkedinscraper.config;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,14 +11,12 @@ public class ScraperConfig {
     public WebDriver setupWebDriver(boolean headlessMode) {
 
         WebDriverManager.chromedriver().setup();
-        WebDriver webDriver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.EAGER); // implies that the WebDriver will wait for the entire page to load before moving on to the next step in the code.
 
-        if(headlessMode) {
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--headless");
-            webDriver = new ChromeDriver(options);
-        }
+        if(headlessMode) options.addArguments("--headless=new");
 
+        WebDriver webDriver = new ChromeDriver(options);
         return webDriver;
     }
 }
